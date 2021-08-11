e39f3242cc1fbcd4deacd7f0698b33b66656e1fe
Baldean Tudor <tbaldean@yahoo.ro>
2021-08-11 16:27:33 +0300

    Implement LCD display and left and right buttons

--- CODE/chess-clock/chess-clock.ino
+++ CODE/chess-clock/chess-clock.ino
@@ -7,19 +7,101 @@
 #define D6 6
 #define D7 7
 
+#define LEFT_PLAYER_BUTTON A0
+#define RIGHT_PLAYER_BUTTON A1
+
+struct Buttons
+{
+  // First - is currently pressed; second - was previously pressed
+  bool left_player[2] = { false, false };
+  bool right_player[2] = { false, false };
+};
+
+enum class Player
+{
+  Left, Right
+};
+
 LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+
+unsigned long left_player_time = 0;  // In seconds
+unsigned long right_player_time = 0;
+unsigned long last_time = 0;  // In milliseconds
+
+Player player = Player::Left;
+
+Buttons buttons;
+
+void display_time(int x, int y, unsigned long player_time)
+{
+  char ascii_zero = '0';
+  char output[7];
+
+  unsigned long minutes = player_time / 60;
+  unsigned long seconds = player_time % 60;
+
+  output[0] = ascii_zero + minutes / 100;
+  output[1] = ascii_zero + (minutes / 10) % 10;
+  output[2] = ascii_zero + minutes % 10;
+  output[3] = ':';
+  output[4] = ascii_zero + seconds / 10;
+  output[5] = ascii_zero + seconds % 10;
+
+  output[6] = 0;
+
+  lcd.setCursor(x, y);
+  lcd.print(output);
+}
+
+bool is_button_pressed(bool* button)
+{
+  if (button[0] && !button[1])
+    return true;
+  else
+    return false;
+}
 
 void setup()
 {
   lcd.begin(16, 2);
 
-  lcd.setCursor(0, 0);
-  lcd.print("Hello, world!");
-  
-  lcd.setCursor(0, 1);
+  lcd.setCursor(7, 0);
+  lcd.print("||");
+  lcd.setCursor(7, 1);
+  lcd.print("||");
 }
 
 void loop()
 {
-  lcd.print(millis() / 1000);
+  buttons.left_player[1] = buttons.left_player[0];
+  buttons.left_player[0] = digitalRead(LEFT_PLAYER_BUTTON);
+  buttons.right_player[1] = buttons.right_player[0];
+  buttons.right_player[0] = digitalRead(RIGHT_PLAYER_BUTTON);
+
+  if (is_button_pressed(buttons.left_player) && player == Player::Left)
+  {
+    player = Player::Right;
+  }
+  else if (is_button_pressed(buttons.right_player) && player == Player::Right)
+  {
+    player = Player::Left;
+  }
+
+  if (millis() - last_time > 1000)
+  {
+    if (player == Player::Left)
+      left_player_time++;
+    else
+      right_player_time++;
+
+    last_time = millis();
+  }
+
+  display_time(0, 1, left_player_time);
+  display_time(10, 1, right_player_time);
+
+//  lcd.setCursor(0, 0);
+//  lcd.print(millis() / 1000);
+
+  delay(5);
 }
